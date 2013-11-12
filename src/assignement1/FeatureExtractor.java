c1debda2bdfa60217292c488896211ade30e4272
pavanbanakar <pavancbz1@gmail.com>
2013-11-11 21:10:01 -0500

    Added Crossing Feature extraction Method

--- src/assignement1/FeatureExtractor.java
+++ src/assignement1/FeatureExtractor.java
@@ -41,5 +41,48 @@ public class FeatureExtractor {
         objNewVector.add(m_VerticalFeatureVector);
  return objNewVector;
      }
+	 
+	 public static Vector<int[]> CrossingFeatureExtraction(int[][] result, int NumberOfRowsOriginal, int NumberOfColumnsOriginal)
+     {
+		 int[] m_HorizontalFeatureVector = new int[NumberOfRowsOriginal];
+         int[] m_VerticalFeatureVector = new int[NumberOfColumnsOriginal];
+         for (int i = 0; i < NumberOfRowsOriginal; i++)
+         {
+             int count = 0;
+             for (int j = 0; j < NumberOfColumnsOriginal; j++)
+             {
+                 if (j != 0)
+                 {
+                     if (result[i][j - 1] == 1 && result[i][j] == 0)
+                     {
+                         count++;
+                     }
+                 }
+             }
+             m_HorizontalFeatureVector[i] = count;
+         }
+
+         for (int j = 0; j < NumberOfColumnsOriginal; j++)
+         {
+             int count = 0;
+             for (int i = 0; i < NumberOfRowsOriginal; i++)
+             {
+                 if (i != 0)
+                 {
+                     if (result[i-1][j] == 1 && result[i][j] == 0)
+                     {
+                         count++;
+                     }
+                 }
+
+             }
+             m_VerticalFeatureVector[j] = count;
+         }
+		 
+		 Vector<int[]> objNewVector = new Vector<int[]>();
+	        objNewVector.add(m_HorizontalFeatureVector);
+	        objNewVector.add(m_VerticalFeatureVector);
+	 return objNewVector;
+     } 
 
 }
